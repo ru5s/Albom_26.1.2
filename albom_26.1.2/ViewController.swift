4c7ccc930a04ec4a38bbf0d5f7fd7327d04f3d7f
ru5s <hellperock@gmail.com>
2022-08-21 20:30:43 +0500

    Initial Commit

---
+++ albom_26.1.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  albom_26.1.2
+//
+//  Created by Ruslan Ismailov on 21/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
